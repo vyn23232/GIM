b90cb700155fe80381f635765e4d5a4e49264e4a
Choovyy <tovijoshua.hermosisima@cit.edu>
2024-11-23 17:35:19 +0800

    Registered the models

--- GIM_apurado/apurado/accounts/admin.py
+++ GIM_apurado/apurado/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Dashboard, Membership, Booking, Class, Attendance, Payment
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Dashboard)
+admin.site.register(Membership)
+admin.site.register(Booking)
+admin.site.register(Class)
+admin.site.register(Attendance)
+admin.site.register(Payment)
