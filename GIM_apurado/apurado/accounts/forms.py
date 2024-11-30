b8a4bb9a74ddedd2f0eea56f96496597d909ae74
vyn <jhovynn3@gmail.com>
2024-11-30 19:46:55 +0800

    book trainer ui changes

--- GIM_apurado/apurado/accounts/forms.py
+++ GIM_apurado/apurado/accounts/forms.py
@@ -1,11 +1,44 @@
-# forms.py
 from django import forms
 from .models import Class
 
 class BookTrainerForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    date = forms.DateField(widget=forms.SelectDateWidget)
-    time = forms.TimeField(widget=forms.TimeInput)
-    class_id = forms.ModelChoiceField(queryset=Class.objects.all(), label="Class")
-    status = forms.CharField(widget=forms.HiddenInput(), initial='Pending')
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter your full name',
+            'class': 'form-control',
+        }),
+        label="Full Name"
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Enter your email address',
+            'class': 'form-control',
+        }),
+        label="Email Address"
+    )
+    date = forms.DateField(
+        widget=forms.SelectDateWidget(attrs={
+            'class': 'form-control',
+        }),
+        label="Preferred Date"
+    )
+    time = forms.TimeField(
+        widget=forms.TimeInput(attrs={
+            'placeholder': 'HH:MM (24-hour format)',
+            'type': 'time',  # Use HTML5 time input
+            'class': 'form-control',
+        }),
+        label="Preferred Time"
+    )
+    class_id = forms.ModelChoiceField(
+        queryset=Class.objects.all(),
+        label="Class",
+        widget=forms.Select(attrs={
+            'class': 'form-control',
+        })
+    )
+    status = forms.CharField(
+        widget=forms.HiddenInput(),
+        initial='Pending'
+    )
